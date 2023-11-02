4089dbe5c6fda7a2b0bf0cfaa6cc83e519d4dc0b
Louis Digon <digon@wisc.edu>
2023-11-02 12:44:46 -0500

    Update

--- app/src/main/java/com/cs407/lab6milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab6milestone1/MainActivity.java
@@ -1,13 +1,18 @@
 package com.cs407.lab6milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends FragmentActivity {
+    private final LatLng mDestinationLatLng = new LatLng(-33.8523341, 151.2106085);
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager()
                         .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            // code to display marker
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
