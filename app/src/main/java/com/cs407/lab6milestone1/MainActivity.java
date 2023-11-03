5ea99ad64b216e6023cad76f6ddd1a6fdf8aa753
Louis Digon <digon@wisc.edu>
2023-11-03 15:27:30 -0500

    Finished lab

--- app/src/main/java/com/cs407/lab6milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab6milestone1/MainActivity.java
@@ -1,19 +1,30 @@
 package com.cs407.lab6milestone1;
 
 // import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity {
     private final LatLng mDestinationLatLng = new LatLng(43.075855371748055, -89.40390984062344);
     private GoogleMap mMap;
-    // private FusedLocationProviderClient mFusedLocationProviderClient; // Save the instance
+    private FusedLocationProviderClient mFusedLocationProviderClient; // Save the instance
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12; // could've been any number
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +39,43 @@ public class MainActivity extends FragmentActivity {
                     .position(mDestinationLatLng)
                     .title("Destination"));
         });
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        displayMyLocation();
+    }
+    private void displayMyLocation() {
+        // check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        // If not, ask for it
+        if(permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // If permission granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+                Location mLastKnownLocation = task.getResult();
+                Log.v("", "" + task.isSuccessful());
+                Log.v("", "" + (mLastKnownLocation != null));
+                if(task.isSuccessful() && mLastKnownLocation != null) {
+                    mMap.addPolyline(new PolylineOptions().add(
+                            new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                            Log.i("", "" + mLastKnownLocation.getLatitude());
+                            Log.i("", "" + mLastKnownLocation.getLongitude());
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
     }
 }
